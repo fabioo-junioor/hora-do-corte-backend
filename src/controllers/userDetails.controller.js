2262a1d37bedff1f382fbbb686dc3589c90e4138
fabioo.junioor <fj_011@hotmail.com>
2024-10-19 23:57:39 -0300

    feat: created userDetails (getByPk) and added req.body (cep)

--- src/controllers/userDetails.controller.js
+++ src/controllers/userDetails.controller.js
@@ -3,6 +3,40 @@ import { getUserDetailsBySlugModel, getUserDetailsByFkModel,
 
 const dateToday = new Date();
 
+const getUserDetailsByPkController = async (req, res) => {
+    try {
+        const pkUser = req.params.pk;
+       
+        const dataUserDetails = await getUserDetailsByFkModel(pkUser);
+        if(!dataUserDetails){
+            return res.status(502).json({
+                statusCode: 502,
+                message: 'Algo deu errado na conexão!'
+
+            });
+        };
+        if(dataUserDetails.length === 0){
+            return res.status(200).json({
+                statusCode: 200,
+                message: 'O usuário não completou o cadastro!',
+                data: dataUserDetails
+                
+            });
+        };        
+        return res.status(200).json({
+            statusCode: 200,
+            message: 'Dados do usuário!',
+            data: dataUserDetails
+
+        });
+    }catch(error){
+        res.status(500).json({
+            statusCode: 500,
+            message: 'Error ao criar o registro!'
+
+        });
+    };
+};
 const getUserDetailsController = async (req, res) => {
     try {
         const slug = req.params.slug;
@@ -39,7 +73,7 @@ const getUserDetailsController = async (req, res) => {
 };
 const createUserDetailsController = async (req, res) => {
     try {
-        const { name, slug, phone, instagram, image, state, city, street, number, pkUser } = req.body;
+        const { name, slug, phone, instagram, image, cep, state, city, street, number, pkUser } = req.body;
         
         const dataUserDetails = await getUserDetailsByFkModel(pkUser);
         if(!dataUserDetails){
@@ -52,7 +86,7 @@ const createUserDetailsController = async (req, res) => {
         if(dataUserDetails.length !== 0){
             return res.status(200).json({
                 statusCode: 200,
-                message: 'Dados do usuário já existe!'
+                message: 'Dados de usuário já existe!'
                 
             });
         };
@@ -72,7 +106,7 @@ const createUserDetailsController = async (req, res) => {
                 
             });
         };        
-        const dataResult = await createUserDetailsModel(name, slug, phone, instagram, image, state, city, street, number, dateToday, pkUser);
+        const dataResult = await createUserDetailsModel(name, slug, phone, instagram, image, cep, state, city, street, number, dateToday, pkUser);
         if(!dataResult){
             return res.status(502).json({
                 statusCode: 502,
@@ -98,7 +132,7 @@ const createUserDetailsController = async (req, res) => {
 const updateUserDetailsController = async (req, res) => {
     try {
         const pkUser = req.params.pk;
-        const { name, slug, phone, instagram, image, state, city, street, number } = req.body;
+        const { name, slug, phone, instagram, image, cep, state, city, street, number } = req.body;
         
         const dataUserDetails = await getUserDetailsBySlugModel(slug);
         if(!dataUserDetails){
@@ -115,7 +149,7 @@ const updateUserDetailsController = async (req, res) => {
                 
             });
         };
-        const dataResult = await updateUserDetailsModel(name, slug, phone, instagram, image, state, city, street, number, pkUser);
+        const dataResult = await updateUserDetailsModel(name, slug, phone, instagram, image, cep, state, city, street, number, pkUser);
         if(!dataResult){
             return res.status(502).json({
                 statusCode: 502,
@@ -167,6 +201,7 @@ const uploadImage = (req, res) => {
 };*/
 export default {
     getUserDetailsController,
+    getUserDetailsByPkController,
     createUserDetailsController,
     updateUserDetailsController
     
