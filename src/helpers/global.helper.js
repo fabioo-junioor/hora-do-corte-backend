b8cc2eb44d02d01e302afa956bd48dcf7f60864a
fabioo-junioor <fj_011@hotmail.com>
2025-03-14 18:54:20 -0300

    fix: new function to generate timeZone

--- src/helpers/global.helper.js
+++ src/helpers/global.helper.js
@@ -1,6 +1,19 @@
 const getTimeZone = () => {
-    const newDate = new Date().toLocaleString("pt-BR", { timeZone: "America/Sao_Paulo" });
-    return newDate;
+    const now = new Date();
+    const options = { timeZone: 'America/Sao_Paulo', hour12: false };
+    const formatter = new Intl.DateTimeFormat('pt-BR', {
+        timeZone: 'America/Sao_Paulo',
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+
+    });
+    const parts = formatter.formatToParts(now);
+    const dateTimeString = `${parts[4].value}-${parts[2].value}-${parts[0].value} ${parts[6].value}:${parts[8].value}:${parts[10].value}`;
+    return dateTimeString;
 
 };
 
