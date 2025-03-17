6ea503debc9639fed9d8265610d374b6ba891f8b
fabioo-junioor <fcjunior@inf.ufsm.br>
2025-03-17 01:25:50 -0300

    feat: added user authentication route

--- src/core/auth/auth.jwt.js
+++ src/core/auth/auth.jwt.js
@@ -5,37 +5,65 @@ dotenv.config();
 const secret = process.env.SECRET_TOKEN;
 
 const verifyToken = (req, res, next) => {
-    const tokenHeader = req.headers["authorization"];
-    const token = tokenHeader && tokenHeader.split(" ")[1];
+    const token = req.header('Authorization')?.split(' ')[1];
 
     if(!token){
         return res.status(401).json({
             statusCode: 401,
-            message: 'Não autorizado!'
+            message: 'Não autorizado!',
+            data: []
+
         });
     };
     try {
         jwt.verify(token, secret);
         next();
 
-    } catch(err){
+    } catch(error){
         return res.status(500).json({
             statusCode: 500,
-            message: 'Token invalido!'
+            message: 'Token invalido!',
+            data: []
+
         });
     };
 };
-
 const createToken = (email) => {
-    const token = jwt.sign({name: email}, secret, {
-        //expiresIn: 2 * 60
+    const token = jwt.sign({ email: email }, secret, {
+        expiresIn: '15sec'
     });
     return token;
 
 };
+const validAuth = (req, res, next) => {
+    const token = req.header('Authorization')?.split(' ')[1];
+
+    if(!token){
+        return res.status(401).json({
+            statusCode: 401,
+            message: 'Não autorizado!',
+            data: []
+
+        });
+    };
+    jwt.verify(token, secret, (error, user) => {
+        if(error){
+            return res.status(403).json({
+                statusCode: 403,
+                message: 'Token inválido!',
+                data: []
+    
+            });
+        };
+        req.user = token;
+        next();
+
+    });
+};
 
 export {
     verifyToken,
-    createToken
+    createToken,
+    validAuth
 
 };
--- src/routes/routes.user.js
+++ src/routes/routes.user.js
@@ -1,10 +1,17 @@
 import { Router } from 'express';
 const routerUser = Router();
-import { verifyToken } from '../core/auth/auth.jwt.js';
+import { verifyToken, validAuth } from '../core/auth/auth.jwt.js';
 
 import userController from '../controllers/user.controller.js';
 
 // Routes user
+routerUser.get('/authUser', validAuth, (req, res) => {
+    res.status(200).json({
+        statusCode: 200,
+        message: 'Autorizado!',
+        data: []
+    });
+});
 routerUser.post('/login', userController.loginUserController);
 routerUser.post('/create', userController.createUserController);
 routerUser.put('/:pk/update', verifyToken, userController.updateUserController);
