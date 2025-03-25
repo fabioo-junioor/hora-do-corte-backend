13567ac934d3de922bea9b1874211e456212fc40
fabioo-junioor <fj_011@hotmail.com>
2025-03-25 14:55:42 -0300

    fix: correction error limiter

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import * as dotenv from 'dotenv';
-import routes from './routes/index.js';
 import { limiter } from './core/security/performance.js';
+import routes from './routes/index.js';
 
 const app = express();
 
 dotenv.config();
 app.use(cors());
 app.use(express.json());
-app.use(routes);
 app.use(limiter);
+app.use(routes);
 
 export default app;
