596ffe1eba15d615099650acbebc33dfc4e91726
fabioo-junioor <fcjunior@inf.ufsm.br>
2025-03-28 11:37:12 -0300

    refator: added new database

--- src/database/db_horadocorte.sql
+++ src/database/db_horadocorte.sql
@@ -1,203 +1,133 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1:3306
--- Tempo de geração: 22/03/2025 às 05:33
--- Versão do servidor: 9.1.0
--- Versão do PHP: 8.3.14
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+CREATE TABLE `plans` (
+	`pkPlan` INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
+	`name` VARCHAR(10) NOT NULL,
+	`price` VARCHAR(5) NOT NULL,
+	`time` VARCHAR(5) NOT NULL,
+	`description` VARCHAR(255) NOT NULL,
+	`benefits` TEXT(65535) NOT NULL,
+	PRIMARY KEY(`pkPlan`)
+);
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+CREATE TABLE `professional` (
+	`pkProfessional` INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
+	`name` VARCHAR(255) NOT NULL,
+	`image` VARCHAR(50) DEFAULT NULL,
+	`instagram` VARCHAR(100) DEFAULT NULL,
+	`isUnavailable` TINYINT NOT NULL,
+	`isActive` TINYINT NOT NULL,
+	`createdAt` DATETIME NOT NULL,
+	`updatedAt` DATETIME NOT NULL,
+	`fkUser` INTEGER NOT NULL,
+	PRIMARY KEY(`pkProfessional`)
+);
 
---
--- Banco de dados: `db_horadocorte`
---
 
--- --------------------------------------------------------
+CREATE TABLE `professionalschedules` (
+	`pkProfessionalSchedules` INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
+	`schedules` TEXT(65535) NOT NULL,
+	`createdAt` DATETIME NOT NULL,
+	`updatedAt` DATETIME NOT NULL,
+	`fkProfessional` INTEGER NOT NULL UNIQUE,
+	PRIMARY KEY(`pkProfessionalSchedules`)
+);
 
---
--- Estrutura para tabela `plans`
---
 
-DROP TABLE IF EXISTS `plans`;
-CREATE TABLE IF NOT EXISTS `plans` (
-  `pkPlan` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(10) NOT NULL,
-  `price` varchar(5) NOT NULL,
-  `time` varchar(5) NOT NULL,
-  `description` varchar(255) NOT NULL,
-  `benefits` mediumtext NOT NULL,
-  PRIMARY KEY (`pkPlan`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
+CREATE TABLE `professionalservices` (
+	`pkProfessionalServices` INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
+	`services` TEXT(65535) NOT NULL,
+	`createdAt` DATETIME NOT NULL,
+	`updatedAt` DATETIME NOT NULL,
+	`fkProfessional` INTEGER NOT NULL UNIQUE,
+	PRIMARY KEY(`pkProfessionalServices`)
+);
 
---
--- Despejando dados para a tabela `plans`
---
 
-INSERT INTO `plans` (`pkPlan`, `name`, `price`, `time`, `description`, `benefits`) VALUES
-(1, 'Free', '0', '30', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.', '[\'30 dias de uso grátis\', \'Suporte diário\']'),
-(2, 'Basic', '25', '30', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.', '[\'30 dias de uso\', \'Suporte diário\']'),
-(3, 'Pro', '135', '180', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.', '[\'180 dias de uso\', \'Suporte diário\']');
+CREATE TABLE `purchaseplanuser` (
+	`pkPurchasePlanUser` INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
+	`purchaseDate` VARCHAR(15) NOT NULL,
+	`purchaseTime` VARCHAR(10) NOT NULL,
+	`purchaseValidity` VARCHAR(15) NOT NULL,
+	`namePlan` VARCHAR(10) NOT NULL,
+	`pricePlan` VARCHAR(5) NOT NULL,
+	`timePlan` VARCHAR(5) NOT NULL,
+	`descriptionPlan` VARCHAR(255) NOT NULL,
+	`benefitsPlan` TEXT(65535) NOT NULL,
+	`createdAt` DATETIME NOT NULL,
+	`fkUser` INTEGER NOT NULL,
+	PRIMARY KEY(`pkPurchasePlanUser`)
+);
 
--- --------------------------------------------------------
 
---
--- Estrutura para tabela `professional`
---
+CREATE TABLE `reservation` (
+	`pkReservation` INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
+	`nameCustomer` VARCHAR(100) NOT NULL,
+	`emailCustomer` VARCHAR(100) DEFAULT NULL,
+	`phoneCustomer` VARCHAR(15) NOT NULL,
+	`observationCustomer` VARCHAR(255),
+	`dateReservation` VARCHAR(15) NOT NULL,
+	`timeReservation` VARCHAR(10) NOT NULL,
+	`services` TEXT(65535) NOT NULL,
+	`price` VARCHAR(5) NOT NULL,
+	`duration` VARCHAR(5) NOT NULL,
+	`isReservation` TINYINT NOT NULL,
+	`createdAt` DATETIME NOT NULL,
+	`updatedAt` DATETIME NOT NULL,
+	`fkUser` INTEGER NOT NULL,
+	`fkProfessional` INTEGER NOT NULL,
+	PRIMARY KEY(`pkReservation`)
+);
 
-DROP TABLE IF EXISTS `professional`;
-CREATE TABLE IF NOT EXISTS `professional` (
-  `pkProfessional` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `image` varchar(50) DEFAULT NULL,
-  `instagram` varchar(100) DEFAULT NULL,
-  `isUnavailable` tinyint NOT NULL,
-  `isActive` tinyint NOT NULL,
-  `createdAt` datetime NOT NULL,
-  `updatedAt` datetime NOT NULL,
-  `fkUser` int NOT NULL,
-  PRIMARY KEY (`pkProfessional`),
-  KEY `fkUser` (`fkUser`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
 
--- --------------------------------------------------------
+CREATE TABLE `user` (
+	`pkUser` INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
+	`email` VARCHAR(100) NOT NULL,
+	`password` VARCHAR(255) NOT NULL,
+	`isActive` TINYINT NOT NULL,
+	`isBlocked` TINYINT NOT NULL,
+	`createdAt` DATETIME NOT NULL,
+	`updatedAt` DATETIME NOT NULL,
+	PRIMARY KEY(`pkUser`)
+);
 
---
--- Estrutura para tabela `professionalschedules`
---
 
-DROP TABLE IF EXISTS `professionalschedules`;
-CREATE TABLE IF NOT EXISTS `professionalschedules` (
-  `pkProfessionalSchedules` int NOT NULL AUTO_INCREMENT,
-  `schedules` mediumtext NOT NULL,
-  `createdAt` datetime NOT NULL,
-  `updatedAt` datetime NOT NULL,
-  `fkProfessional` int NOT NULL,
-  PRIMARY KEY (`pkProfessionalSchedules`),
-  KEY `fkProfessional` (`fkProfessional`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
+CREATE TABLE `userdetails` (
+	`pkUserDetails` INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
+	`name` VARCHAR(100) NOT NULL,
+	`slug` VARCHAR(50) NOT NULL,
+	`phone` VARCHAR(15) NOT NULL,
+	`instagram` VARCHAR(100) DEFAULT NULL,
+	`image` VARCHAR(50) DEFAULT NULL,
+	`cep` VARCHAR(15) NOT NULL,
+	`state` VARCHAR(100) NOT NULL,
+	`city` VARCHAR(100) NOT NULL,
+	`street` VARCHAR(100) NOT NULL,
+	`number` VARCHAR(20) DEFAULT NULL,
+	`createdAt` DATETIME NOT NULL,
+	`updatedAt` DATETIME NOT NULL,
+	`fkUser` INTEGER NOT NULL UNIQUE,
+	PRIMARY KEY(`pkUserDetails`)
+);
 
--- --------------------------------------------------------
 
---
--- Estrutura para tabela `professionalservices`
---
-
-DROP TABLE IF EXISTS `professionalservices`;
-CREATE TABLE IF NOT EXISTS `professionalservices` (
-  `pkProfessionalServices` int NOT NULL AUTO_INCREMENT,
-  `services` mediumtext NOT NULL,
-  `createdAt` datetime NOT NULL,
-  `updatedAt` datetime NOT NULL,
-  `fkProfessional` int NOT NULL,
-  PRIMARY KEY (`pkProfessionalServices`),
-  KEY `fkProfessional` (`fkProfessional`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `purchaseplanuser`
---
-
-DROP TABLE IF EXISTS `purchaseplanuser`;
-CREATE TABLE IF NOT EXISTS `purchaseplanuser` (
-  `pkPurchasePlanUser` int NOT NULL AUTO_INCREMENT,
-  `purchaseDate` varchar(15) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
-  `purchaseTime` varchar(10) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
-  `purchaseValidity` varchar(15) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
-  `namePlan` varchar(10) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
-  `pricePlan` varchar(5) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
-  `timePlan` varchar(5) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
-  `descriptionPlan` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
-  `benefitsPlan` mediumtext NOT NULL,
-  `createdAt` datetime NOT NULL,
-  `fkUser` int NOT NULL,
-  PRIMARY KEY (`pkPurchasePlanUser`),
-  KEY `fkUser` (`fkUser`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `reservation`
---
-
-DROP TABLE IF EXISTS `reservation`;
-CREATE TABLE IF NOT EXISTS `reservation` (
-  `pkReservation` int NOT NULL AUTO_INCREMENT,
-  `nameCustomer` varchar(100) NOT NULL,
-  `emailCustomer` varchar(100) DEFAULT NULL,
-  `phoneCustomer` varchar(15) NOT NULL,
-  `observationCustomer` varchar(255) DEFAULT NULL,
-  `dateReservation` varchar(15) NOT NULL,
-  `timeReservation` varchar(10) NOT NULL,
-  `services` mediumtext NOT NULL,
-  `price` varchar(5) NOT NULL,
-  `duration` varchar(5) NOT NULL,
-  `isReservation` tinyint NOT NULL,
-  `createdAt` datetime NOT NULL,
-  `updatedAt` datetime NOT NULL,
-  `fkUser` int NOT NULL,
-  `fkProfessional` int NOT NULL,
-  PRIMARY KEY (`pkReservation`),
-  KEY `fkProfessional` (`fkProfessional`),
-  KEY `fkUser` (`fkUser`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `user`
---
-
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user` (
-  `pkUser` int NOT NULL AUTO_INCREMENT,
-  `email` varchar(100) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `isActive` tinyint NOT NULL,
-  `isBlocked` tinyint NOT NULL,
-  `createdAt` datetime NOT NULL,
-  `updatedAt` datetime NOT NULL,
-  PRIMARY KEY (`pkUser`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `userdetails`
---
-
-DROP TABLE IF EXISTS `userdetails`;
-CREATE TABLE IF NOT EXISTS `userdetails` (
-  `pkUserDetails` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
-  `slug` varchar(50) NOT NULL,
-  `phone` varchar(15) NOT NULL,
-  `instagram` varchar(100) DEFAULT NULL,
-  `image` varchar(50) DEFAULT NULL,
-  `cep` varchar(15) NOT NULL,
-  `state` varchar(100) NOT NULL,
-  `city` varchar(100) NOT NULL,
-  `street` varchar(100) NOT NULL,
-  `number` varchar(20) DEFAULT NULL,
-  `createdAt` datetime NOT NULL,
-  `updatedAt` datetime NOT NULL,
-  `fkUser` int NOT NULL,
-  PRIMARY KEY (`pkUserDetails`),
-  KEY `fkUser` (`fkUser`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+ALTER TABLE `userdetails`
+ADD FOREIGN KEY(`fkUser`) REFERENCES `user`(`pkUser`)
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+ALTER TABLE `purchaseplanuser`
+ADD FOREIGN KEY(`fkUser`) REFERENCES `user`(`pkUser`)
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+ALTER TABLE `professional`
+ADD FOREIGN KEY(`fkUser`) REFERENCES `user`(`pkUser`)
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+ALTER TABLE `professionalschedules`
+ADD FOREIGN KEY(`fkProfessional`) REFERENCES `professional`(`pkProfessional`)
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+ALTER TABLE `reservation`
+ADD FOREIGN KEY(`fkProfessional`) REFERENCES `professional`(`pkProfessional`)
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+ALTER TABLE `reservation`
+ADD FOREIGN KEY(`fkUser`) REFERENCES `user`(`pkUser`)
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+ALTER TABLE `professionalservices`
+ADD FOREIGN KEY(`fkProfessional`) REFERENCES `professional`(`pkProfessional`)
+ON UPDATE NO ACTION ON DELETE NO ACTION;
